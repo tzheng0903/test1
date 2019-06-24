b6877245ddccf43e8d641de391d72fcf0e6e9583
Tao.Zheng1 <Tao.Zheng1@geely.com>
2019-06-24 20:57:05 +0800

    sdf

--- src/main/java/cn/evun/kafka/t1/T1Application.java
+++ src/main/java/cn/evun/kafka/t1/T1Application.java
@@ -3,7 +3,7 @@ package cn.evun.kafka.t1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,30 +21,13 @@ public class T1Application {
         SpringApplication.run(T1Application.class, args);
     }
 
-    @Autowired
-    private KafkaTemplate kafkaTemplate;
+//    @Autowired
+//    private KafkaTemplate kafkaTemplate;
 
     @GetMapping("/test")
-    public Object test() throws InterruptedException, ExecutionException {
+    public Object test() {
 
-        Executor executor = Executors.newFixedThreadPool(100);
-        CountDownLatch cdl = new CountDownLatch(100);
-        for(int i = 0;i<100;i++){
-            executor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        cdl.await();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    for(int i = 0 ;i<1000;i++){
-                        ListenableFuture send = kafkaTemplate.send("my-topic", "data:" + System.currentTimeMillis());
-                    }
-                }
-            });
-            cdl.countDown();
-        }
+
         return "OK";
     }
 
--- src/main/java/cn/evun/kafka/t1/service/KafkaServcie.java
+++ src/main/java/cn/evun/kafka/t1/service/KafkaServcie.java
@@ -1,6 +1,6 @@
 package cn.evun.kafka.t1.service;
 
-import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.atomic.AtomicInteger;
@@ -10,7 +10,7 @@ public class KafkaServcie {
 
     AtomicInteger i = new AtomicInteger(0);
 
-    @KafkaListener(topics = "my-topic")
+//    @KafkaListener(topics = "my-topic")
     public void listnerMethod(String content){
         i.incrementAndGet();
         System.out.println( i +content);
